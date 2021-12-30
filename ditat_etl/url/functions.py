4b66a69c189d24887767751472ee88143456dcf0
Tomas Silva <tomas.silva@acepta.com>
2021-12-30 19:59:07 -0300

    fix extract_domain

--- ditat_etl/url/functions.py
+++ ditat_etl/url/functions.py
@@ -8,16 +8,19 @@ from ..utils.time_functions import time_it
 
 
 def extract_domain(url_or_email):
-	url_or_email = str(url_or_email)
-	if '@' in url_or_email:
-		regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-		if re.fullmatch(regex, url_or_email):
-			domain = url_or_email.split('@')[1]
-			return domain
-	else:
-		url_or_email = f'http://{url_or_email}' if not url_or_email.startswith('http') else url_or_email
-		domain = urlparse(url_or_email.replace('www.', '')).netloc
-		return domain
+    url_or_email = str(url_or_email)
+    if '@' in url_or_email:
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        if re.fullmatch(regex, url_or_email):
+            domain = url_or_email.split('@')[1]
+            print(domain)
+            if domain:
+                return domain
+    else:
+        url_or_email = f'http://{url_or_email}' if not url_or_email.startswith('http') else url_or_email
+        domain = urlparse(url_or_email.replace('www.', '')).netloc
+        if domain:
+            return domain
 
 
 @time_it()
